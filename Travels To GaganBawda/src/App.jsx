80845c5b5dbb8e2e7b08a2fd8583c222766c767f
prachi shinde <shindeprachi1960-coder-github-@gamil.com>
2026-06-12 13:12:14 +0530

    Added GaganBawda website updates

--- Travels To GaganBawda/src/App.jsx
+++ Travels To GaganBawda/src/App.jsx
@@ -1,20 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
-import Home from "./pages/Home"
-import About from "./pages/About"
-import SubviewPoints from "./pages/SubviewPoints"
-import Booking from "./pages/Booking"
+import { Routes, Route } from "react-router-dom"
+import Home from "./Pages/Home"
+import About from "./Pages/About"
+import SubviewPoints from "./Pages/SubviewPoints"
+import Booking from "./Pages/Booking"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/subviewpoints" element={<SubviewPoints />} />
-        <Route path="/booking" element={<Booking />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/subviewpoints" element={<SubviewPoints />} />
+      <Route path="/booking" element={<Booking />} />
+    </Routes>
   )
 }
 
